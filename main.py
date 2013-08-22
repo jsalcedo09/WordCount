6488e8274bf60fec11ff009c5b54ef24948ca425
jorge.salcedo@eforcers.com <jorge.salcedo@eforcers.com>
2013-08-22 17:20:55 -0500

    Added channels feedback

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import logging
 import os
+import random
 from google.appengine.ext import deferred
+from google.appengine.api import channel, users, memcache
 import jinja2
 import webapp2
 from tasks import read_web_site
+import string
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = \
@@ -12,18 +14,28 @@ jinja_environment = \
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        context = {}
-
-
+        string.letters
+        context = {'search_id':''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(5))}
         template = jinja_environment.get_template('index.html')
         self.response.write(template.render(context))
 
     def post(self):
-        context = {}
+        context = dict()
+
         context['url'] = self.request.POST.get("url")
-        context['deep'] = self.request.POST.get("deep")
+        context['search_id'] = self.request.POST.get("search_id")
+        context['deep'] = int(self.request.POST.get("deep"))
         context['words'] = self.request.POST.get("words")
-        deferred.defer(read_web_site,context['url'],int(context['deep']), context['words'])
+
+        #Clear counters
+        memcache.set('%s-links' % context['search_id'], 0)
+        for word in context['words'].split(','):
+            memcache.set("%s-word-%s" % (context['search_id'], word),0)
+
+        deferred.defer(read_web_site, **context)
+
+        context['token'] = channel.create_channel("%s-search" % context['search_id'])
+
         template = jinja_environment.get_template('index.html')
         self.response.write(template.render(context))
 
--- tasks.py
+++ tasks.py
@@ -3,34 +3,50 @@ import urllib
 import re
 from google.appengine.ext import deferred
 from google.appengine.api import memcache
+from google.appengine.api import channel
+import json
 
 
-
-def read_web_site(url, deep, words, visited = list()):
+def read_web_site(url, deep, words, search_id,visited = list()):
     deep -= 1
     visited.append(url)
-    increment_links(1)
+    increment_links(search_id,1)
+    information = {'links':memcache.get('%s-links' % search_id),
+                   'words':{}}
+
     logging.info("Searching for links in %s", url)
+
     if deep >= 0:
         req = urllib.urlopen(url)
         encoding=req.headers['content-type'].split('charset=')[-1]
-        html = unicode(req.read(),encoding)
-        for word in words.split(','):
-            word_count = html.count(word)
-            if word_count:
-                increment_word(word_count, word)
+        try:
+            html = unicode(req.read(), encoding)
+            for word in words.split(','):
+                word_count = html.count(word)
+                if word_count:
+                    increment_word(search_id, word_count, word)
 
-        for link in find_links(html):
-            if link.startswith('http') and link not in visited:
-                deferred.defer(read_web_site, link, deep, words, visited)
-    else:
-        pass
+
+            for link in find_links(html):
+                if link.startswith('http') and link not in visited:
+                    deferred.defer(read_web_site, link, deep, words, search_id, visited)
+        except LookupError:
+            logging.warn("Invalid ecoding %s", encoding)
+
+
+    for word in words.split(','):
+        information['words'].update({word:memcache.get("%s-word-%s" % (search_id, word))})
+
+    channel.send_message("%s-search" % search_id, json.dumps(information))
+
+
+
 
 def find_links(html):
     return re.findall(r'href=[\'"]?([^\'" >]+)', html)
 
-def increment_links(number):
-    memcache.incr("links", delta=number, initial_value=0)
+def increment_links(search_id, number):
+    memcache.incr("%s-links" % search_id, delta=number, initial_value=0)
 
-def increment_word(number,word):
-    memcache.incr("word-%s" % word, delta=number, initial_value=0)
+def increment_word(search_id, number,word):
+    memcache.incr("%s-word-%s" % (search_id, word), delta=number, initial_value=0)
